8f7cd8d64daa909c28a258e8ceda2a3cc9b2b969
KaraAi <116943226+KaraAi@users.noreply.github.com>
2024-09-07 16:51:46 +0700

    Delete app/chart/index.tsx

--- app/chart/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ChartPage from "./page";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChartPage />
-  </React.StrictMode>
-);
